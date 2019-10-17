c9a9fe7a2b88624f501ec8ca4f2b6d1c4b07ae6c
Naveen Kumar S P <NaveenKumar_SP@ad.infosys.com>
2019-10-17 15:29:15 +0530

    initial commit

---
+++ lab-02/ad-hoc-commands-on-inventory.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#eval `ssh-agent`
+#ssh-add /vagrant/keys/key
+
+ansible -u vagrant -m ping web1
+ansible -u vagrant -m ping app
+ansible -u vagrant -m ping webservers
+ansible -u vagrant -m ping dc
+ansible -u vagrant -m shell -a 'ls -al' web1
+ansible -u vagrant -m shell -a 'whoami' app
+ansible -u vagrant -m shell -a 'ifconfig' webservers
+ansible -u vagrant -m shell -a 'hostname' dc
+ansible -u vagrant -m ping all
+ansible -u vagrant -m ping web*
+ansible -u vagrant -m ping 'appservers:dbservers'
+ansible -u vagrant -m ping 'dc:!webservers'
+ansible -u vagrant -m ping 'dc:&webservers'
